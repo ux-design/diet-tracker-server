2af977490fd979346ecad6fc4b640ad7c5e0eeb0
Andrea Mazzilli <andreamazzilli@Andreas-MacBook-Pro.local>
2018-06-22 21:32:56 +0200

    assets

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 // FILE SYSTEM
+
 const fs = require('fs')
+
 // SERVER
+
 const express = require('express')
 const ip = require('ip').address()
 const cors = require('cors')
@@ -10,24 +13,52 @@ var bodyParser = require('body-parser');
 const http = require('http')
 const serverHTTP = http.createServer(app)
 const port = 4000
-// API
+
+// SERVER STATE
+
+const state = {
+  food: JSON.parse(fs.readFileSync('./db/food.json')),
+  users: JSON.parse(fs.readFileSync('./db/users.json'))
+}
+
+// HELPERS
+
+const _logMemUsage = () => {
+  console.log(process.memoryUsage().heapUsed / 1024 / 1024)
+}
+
+// API CONFIG
+
 app.use(compression())
 app.use(cors())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
-// API TEST
-app.get('/api/:api', function(req, res) {
+
+// API GET
+
+app.get('/api/:api', (req, res) => {
+  _logMemUsage()
   const {api} = req.params
   switch(api) {
     case"food":
-      const food = JSON.parse(fs.readFileSync('./db/food.json'))
-      res.send({"success":food})
+      res.send({ "success": state.food })
       break
     default:
-      res.send({"error":"page not found"})
+      res.send({ "error": "page not found "})
   }
 })
-app.post('/api/:api', function(req, res) {
+
+app.get('/assets/food/:name', (req, res) => {
+  const {name} = req.params;
+  console.log(`${name}.svg`)
+  _logMemUsage()
+  res.sendFile( __dirname + `/assets/food/${name}.svg` );
+} ) ;
+
+// API POST
+
+app.post('/api/:api', (req, res) => {
+  _logMemUsage()
   const {api} = req.params
   const data = req.body
   const users = JSON.parse(fs.readFileSync('./db/users.json'))
@@ -35,18 +66,20 @@ app.post('/api/:api', function(req, res) {
     case"login":
       if (users[data.email]) {
         if (users[data.email].password === data.password) {
-          res.send({"success":users[data.email]})
+          res.send({ "success": users[ data.email ] })
         } else {
-          res.send({"error":"wrong credentials"})
+          res.send({ "error": "wrong credentials" })
         }
       } else {
-        res.send({"error":"wrong credentials"})
+        res.send({ "error": "wrong credentials" })
       }      
       break
     default:
-      res.send({"error":"page not found"})
+      res.send({ "error": "page not found" })
   }
 })
+
 // INIT
+
 serverHTTP.listen(port)  
 console.log(`http://${ip}:${port}`)
